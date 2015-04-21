d4a07da944f8e69f1f21a1a2b8960dd82079efa2
Alexander Deynichenko <adeynichenko@gmail.com>
2015-04-22 06:42:38 +0700

    added caching

--- main.py
+++ main.py
@@ -5,10 +5,15 @@ import re
 import json
 
 from flask import Flask, request, render_template
-from flask_hipchat_addon.addon import Addon, db
+from flask_hipchat_addon.addon import Addon, db, cache
 from flask_hipchat_addon.clients import RoomClient
 from flask_hipchat_addon.auth import tenant, sender
+from flask_hipchat_addon.events import events
 from closeio_api import Client as CloseIO_API, APIError
+
+app = Flask(__name__)
+app.config.from_object('settings')
+addon = Addon(app=app, allow_global=True, scopes=['send_notification'])
 
 
 class CloseIOApi(db.Model):
@@ -21,11 +26,17 @@ class CloseIOApi(db.Model):
         self.api_key = api_key
 
 
-app = Flask(__name__)
-app.config.from_object('settings')
-addon = Addon(app=app, allow_global=True, scopes=['send_notification'])
+@cache.memoize(timeout=3600)
+def get_api_key(tenant_id):
+    close_io_api = CloseIOApi.query.filter_by(tenant_id=tenant_id).first()
+    return close_io_api.api_key
+
+def get_lead_info(api, lead_id):
+    lead = api.get('lead/'+lead_id, data={'_fields': 'id,display_name'})
+    return lead
 
 LEAD_REGEXP = re.compile(ur'.*(https://app\.close\.io\/lead\/)(lead_[a-zA-Z0-9]+)', re.MULTILINE | re.UNICODE)
+
 
 @addon.webhook(event='room_message', pattern='https:\/\/app\.close\.io\/lead\/lead_[a-zA-Z0-9]+')
 def room_message_hook():
@@ -37,16 +48,16 @@ def room_message_hook():
     if matched:
         lead_url = matched.group(1) + matched.group(2)
         lead_id = matched.group(2)
-        api = CloseIO_API(app.config['CLOSE_IO_API_KEY'])
-        resp = api.get('lead/'+lead_id, data={'_fields': 'id,display_name'})
-        notification = '<a href="'+lead_url+'">'+lead_url+'</a><br/><b>'+resp['display_name']+'</b>'
+        api = CloseIO_API(get_api_key(tenant.id))
+        lead = get_lead_info(api, lead_id)
+        notification = '<a href="'+lead_url+'">'+lead_url+'</a><br/><b>'+lead['display_name']+'</b>'
         room_client = RoomClient(room_id)
         room_client.send_notification(notification)
     return '', 204
 
+
 @addon.configure_page(methods=['GET'])
 def configure_page(error=None, success=None):
-    app.logger.debug('tenant: %s', tenant)
     close_io_api = CloseIOApi.query.filter_by(tenant_id=tenant.id).first()
     api_key = ''
     if close_io_api:
@@ -54,20 +65,32 @@ def configure_page(error=None, success=None):
     return render_template('configure.html', token=tenant.sign_jwt(sender.id),
                            api_key=api_key, success=success), 200
 
+
 @addon.configure_page(methods=['POST'])
 def save_configuration():
-    app.logger.debug('tenant: %s', tenant)
-    new_api_key = request.form['api_key'].strip()
-    close_io_api = CloseIOApi.query.filter_by(tenant_id=tenant.id).first()
-    if not close_io_api:
-        close_io_api = CloseIOApi(tenant.id, new_api_key)
-    else:
-        close_io_api.api_key = new_api_key
-    db.session.add(close_io_api)
+    new_api_key = request.form.get('apikey')
+    cache.delete_memoized(get_api_key, tenant.id)
+    if new_api_key is not None:
+        new_api_key = new_api_key.strip()
+        close_io_api = CloseIOApi.query.filter_by(tenant_id=tenant.id).first()
+        if not close_io_api:
+            close_io_api = CloseIOApi(tenant.id, new_api_key)
+        else:
+            close_io_api.api_key = new_api_key
+        db.session.add(close_io_api)
+        db.session.commit()
+
+    return configure_page(success='New key was saved')
+
+
+def on_uninstall(event):
+    cache.delete_memoized(get_api_key, event['client'].id)
+    close_io_api = CloseIOApi.query.filter_by(tenant_id=event['client'].id).first()
+    db.session.delete(close_io_api)
     db.session.commit()
 
-    return configure_page(success='New key saved')
-
-
 if __name__ == '__main__':
+    with app.app_context():
+        cache.clear()
+    events.register_event("uninstall", on_uninstall)
     addon.run(debug=True)
