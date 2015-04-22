9a5dd014a15be28fb9d03395ea225fbe6d1a0f8f
Alexander Deynichenko <adeynichenko@gmail.com>
2015-04-23 06:17:48 +0700

    added port detection

--- main.py
+++ main.py
@@ -101,4 +101,6 @@ if __name__ == '__main__':
     with app.app_context():
         cache.clear()
     events.register_event("uninstall", on_uninstall)
-    addon.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    addon.run(host='0.0.0.0', port=port, debug=True)
+
