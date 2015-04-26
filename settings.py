bc44f95bc2ec44d2d12910e0e5a0fe5f58cd2e98
Alexander Deynichenko <adeynichenko@gmail.com>
2015-04-27 06:03:07 +0700

    LICENSE, uncommented lines in settings

--- settings.py
+++ settings.py
@@ -1,10 +1,10 @@
 #SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
 
-#HIPCHAT_ADDON_KEY = 'closeio-hipchat'
-#HIPCHAT_ADDON_NAME = 'Close.io'
-#HIPCHAT_ADDON_DESCRIPTION = 'A HipChat add-on to give details about a Close.io lead when its URL is mentioned in HipChat'
-#HIPCHAT_ADDON_VENDOR_URL = 'http://close.io'
-#HIPCHAT_ADDON_VENDOR_NAME = 'Close.io'
+HIPCHAT_ADDON_KEY = 'closeio-hipchat'
+HIPCHAT_ADDON_NAME = 'Close.io'
+HIPCHAT_ADDON_DESCRIPTION = 'A HipChat add-on to give details about a Close.io lead when its URL is mentioned in HipChat'
+HIPCHAT_ADDON_VENDOR_URL = 'http://close.io'
+HIPCHAT_ADDON_VENDOR_NAME = 'Close.io'
 #HIPCHAT_ADDON_BASE_URL = 'https://closeio-hipchat.herokuapp.com'
 
 CACHE_TYPE = 'simple'
